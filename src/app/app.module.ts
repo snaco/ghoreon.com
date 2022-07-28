a6ad3cc443fff762f6912d08095b12802e5f84fc
snaco <jm.anderson.cs@gmail.com>
2022-07-28 00:37:26 -0400

    updated readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AbilityComponent } from './ability/ability.component';
+import { AcComponent } from './ac/ac.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AbilityComponent,
+    AcComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
